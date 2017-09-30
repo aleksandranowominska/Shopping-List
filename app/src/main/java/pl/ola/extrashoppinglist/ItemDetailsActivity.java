06fcc03a7d74d95805de10361c60980829ae5db6
Olka Kusiak <olkakusiak@gmail.com>
2017-09-30 15:49:04 +0200

    added hour to reminder

--- app/src/main/java/pl/ola/extrashoppinglist/ItemDetailsActivity.java
+++ app/src/main/java/pl/ola/extrashoppinglist/ItemDetailsActivity.java
@@ -1,6 +1,7 @@
 package pl.ola.extrashoppinglist;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,20 +11,24 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class ItemDetailsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class ItemDetailsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     public static final String ITEM_POSITION = "ITEM_POSITION";
 
     EditText itemNameEditText;
     EditText itemDescriptionEditText;
     TextView itemReminderDateTextView;
+    TextView itemReminderHourTextView;
     DataManager dataManager;
     Item item;
     int position;
+    Calendar reminderCalendar;
 
 
     @Override
@@ -33,11 +38,18 @@ public class ItemDetailsActivity extends AppCompatActivity implements DatePicker
         itemNameEditText = (EditText) findViewById(R.id.item_name_to_edit);
         itemDescriptionEditText = (EditText) findViewById(R.id.item_description);
         itemReminderDateTextView = (TextView) findViewById(R.id.item_reminder_date);
-
-        dataManager = DataManager.getInstance(this);
+        itemReminderHourTextView = (TextView) findViewById(R.id.item_reminder_hour);
         Intent intent = getIntent();
+        dataManager = DataManager.getInstance(this);
         position = intent.getIntExtra(ITEM_POSITION, 0);
         item = dataManager.getItemFromPosition(position);
+        reminderCalendar = Calendar.getInstance();
+        if (item.itemReminderDate != null){
+            reminderCalendar.setTime(item.itemReminderDate);
+        }
+
+
+
         updateUI();
         setupListeners();
     }
@@ -46,10 +58,20 @@ public class ItemDetailsActivity extends AppCompatActivity implements DatePicker
         itemNameEditText.setText(item.itemName);
         itemDescriptionEditText.setText(item.itemDescription);
         if (item.itemReminderDate != null){
-            SimpleDateFormat simpleDate =  new SimpleDateFormat("dd/MM/yyyy");
-            String itemReminderDateString = simpleDate.format(item.itemReminderDate);
+            SimpleDateFormat dateFormat =  new SimpleDateFormat("dd/MM/yyyy");
+            String itemReminderDateString = dateFormat.format(item.itemReminderDate);
             itemReminderDateTextView.setText(itemReminderDateString);
 
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+            String itemReminderHourString = timeFormat.format(item.itemReminderDate);
+            itemReminderHourTextView.setText(itemReminderHourString);
+        }
+
+        if (item.itemReminderDate != null){
+            itemReminderHourTextView.setVisibility(View.VISIBLE);
+        }
+        else {
+            itemReminderHourTextView.setVisibility(View.GONE);
         }
     }
 
@@ -70,6 +92,7 @@ public class ItemDetailsActivity extends AppCompatActivity implements DatePicker
         saveEditedItemName();
         saveEditedItemDescription();
         saveReminderDate();
+        saveReminderHour();
     }
 
     private void saveEditedItemName(){
@@ -116,9 +139,9 @@ public class ItemDetailsActivity extends AppCompatActivity implements DatePicker
         itemReminderDateTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int startYear = Calendar.getInstance().get(Calendar.YEAR);
-                int startMonth = Calendar.getInstance().get(Calendar.MONTH);
-                int startDay = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+                int startYear = reminderCalendar.get(Calendar.YEAR);
+                int startMonth = reminderCalendar.get(Calendar.MONTH);
+                int startDay = reminderCalendar.get(Calendar.DAY_OF_MONTH);
                 DatePickerDialog datePickerDialog = new DatePickerDialog(ItemDetailsActivity.this, ItemDetailsActivity.this, startYear, startMonth, startDay);
                 datePickerDialog.show();
             }
@@ -127,12 +150,31 @@ public class ItemDetailsActivity extends AppCompatActivity implements DatePicker
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month, dayOfMonth);
-        Date itemReminderDateFromDataPicker = calendar.getTime();
-        item.itemReminderDate = itemReminderDateFromDataPicker;
+        reminderCalendar.set(year, month, dayOfMonth);
+        item.itemReminderDate = reminderCalendar.getTime();
         dataManager.updateItem(position, item);
         updateUI();
 
+    }
+
+    private void saveReminderHour(){
+        itemReminderHourTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int startHour = reminderCalendar.get(Calendar.HOUR_OF_DAY);
+                int startMinute = reminderCalendar.get(Calendar.MINUTE);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(ItemDetailsActivity.this, ItemDetailsActivity.this, startHour, startMinute, true);
+                timePickerDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+        reminderCalendar.set(Calendar.HOUR_OF_DAY, hour);
+        reminderCalendar.set(Calendar.MINUTE, minute);
+        item.itemReminderDate = reminderCalendar.getTime();
+        dataManager.updateItem(position, item);
+        updateUI();
     }
 }
